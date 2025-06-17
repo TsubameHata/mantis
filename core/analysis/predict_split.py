00545d77a59153595870b73bc0def54b523b49e6
TsubameHata <2640855495@qq.com>
2025-06-17 20:45:12 +0800

    Fixed predict functions, size_ratio and prominence to be further adjusted.

--- core/analysis/predict_split.py
+++ core/analysis/predict_split.py
@@ -5,7 +5,16 @@ from typing import Iterable
 
 MatLike = cv2.typing.MatLike
 
-KERNEL_SIZE_RATIO = 20
+def gen_sigmoid(t:float, k:float):
+    def f(x: MatLike):
+        return 1.0 / (1.0 + np.exp(k * (x - t)))
+    return f
+
+KERNEL_SIZE_RATIO = 35
+SIGMOID_T = 0.2
+SIGMOID_K = 30
+
+sigmoid = gen_sigmoid(SIGMOID_T, SIGMOID_K)
 
 def split_probability(img: MatLike)->MatLike:
     """Get the probability of each height being the position to split using Gaussian convolution."""
@@ -17,16 +26,21 @@ def split_probability(img: MatLike)->MatLike:
     convolve_kernel = cv2.getGaussianKernel(kernel_size, sigma=kernel_size//4).flatten()
     density_smoothed = np.convolve(density, convolve_kernel, mode="same")
     density_normed = density_smoothed / np.max(density_smoothed)
+
+    density_sig = sigmoid(density_normed)
+    density_sig_normed = density_sig / np.max(density_sig)
+
+    return density_sig_normed
     
-    return density_normed
 
 MIN_PEAK_DISTANCE = 20
-MIN_PEAK_PROMINENCE = 10
+MIN_PEAK_PROMINENCE = 0.05
 
 def find_peaks(probability: MatLike)->Iterable[int]:
     """Find peaks from a density mat."""
     peaks, props = ss.find_peaks(probability, distance=MIN_PEAK_DISTANCE, 
-                             prominence=MIN_PEAK_PROMINENCE)
+                             prominence=MIN_PEAK_PROMINENCE,
+                             width=1)
     lefts = props["left_ips"].astype(int)
     rights = props["right_ips"].astype(int)
     return np.round((lefts+rights)/2)
@@ -39,18 +53,26 @@ def paint_probabilities_and_peaks(img: MatLike,
     probability = split_probability(img)
     peaks = find_peaks(probability)
     
-    dest = np.zeros_like(img, dtype=np.uint8)
+    dst = np.zeros_like(img, dtype=np.uint8)
     alpha_layer = np.zeros_like(img, dtype=np.uint8)
     
+    img_height = img.shape[0]
     img_width = img.shape[1]
     
-    prob_line_length = probability*img_width
+    scaled = (probability*img_width).astype(int)
+    prob_line_length = np.clip(scaled, 0, img_width - 2)
     for y,l in enumerate(prob_line_length):
         cv2.line(alpha_layer, (0,y), (l,y), prob_color)
     
+    peaks = np.clip(peaks, 0, img_height - 2).astype(int)
     for y in peaks:
-        cv2.line(alpha_layer, (0, y), (img_width, y), peak_color)
+        cv2.line(alpha_layer, (0, y), (img_width - 1, y), peak_color)
         
-    cv2.addWeighted(img, 1.0, alpha_layer, alpha, 0, dest)
+    cv2.addWeighted(img, 1.0-alpha, alpha_layer, alpha, 0, dst)
     
-    return dest
+    return dst
+
+def test(input_dir: str, output_dir: str) -> None:
+    img = cv2.imread(input_dir)
+    dst = paint_probabilities_and_peaks(img, alpha=0.5)
+    cv2.imwrite(output_dir, dst)
