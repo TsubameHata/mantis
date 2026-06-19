c789f1b36298685a04510124cd7a3e226e0605bc
TsubameHata <2640855495@qq.com>
2026-06-20 02:18:53 +0800

    Feature: split::compose::ImageComposer, all initialization methods

--- crates/core/src/split/compose.rs
+++ crates/core/src/split/compose.rs
@@ -2,6 +2,13 @@ use image::{DynamicImage, GenericImageView, Rgb};
 
 use crate::split::mask::FinalMask;
 
+/// Defines the action to take when the size of image, after scaling according to the padding, 
+/// is greater than the output canvas.
+pub enum Overflow {
+    Hidden,
+    Shrink
+}
+
 #[derive(Default)]
 pub struct ImageComposer {
     /// The mask to be proceeded.
@@ -24,11 +31,12 @@ pub struct ImageComposer {
     /// Default value: (255, 255, 255).
     background_color: Option<Rgb<u8>>,
 
-    shrink_y_overflow: Option<bool>,
+    overflow_y: Option<Overflow>,
 
-    shrink_x_overflow: Option<bool>
+    overflow_x: Option<Overflow>
 }
 
+// This `impl` block defines the initialization methods, which is all marked `pub`.
 impl ImageComposer {
     /// Set the mask to be proceeded. Panics if the image is already set and the size is inconsistent.
     pub fn mask(mut self, m: FinalMask) -> Self {
@@ -65,14 +73,58 @@ impl ImageComposer {
     }
 
     /// Set the size of the output.
+    /// 
+    /// Default value is `(1920, 1080)`.
     pub fn output_size(mut self, width: usize, height: usize) -> Self {
         self.output_size = Some((width, height));
 
         self
     }
 
+    /// Set the padding of the output, measured from the top border to the beginning of the main area of content.
+    /// 
+    /// This method should be called after the size of the output size is set, 
+    /// and the padding must not be greater than half of the height of the page, otherwise the program panics.
+    /// 
+    /// Default value is `0.08*output_height`.
     pub fn output_padding_y(mut self, padding: usize) -> Self {
-        todo!();
+        assert!(self.output_size.is_some());
+        assert!(2*padding < self.output_size.unwrap().1);
+
+        self.output_padding_y = Some(padding);
+
         self        
     }
+
+    /// Set the background color of the output.
+    /// 
+    /// Default value is `Rgb(255, 255, 255)`.
+    pub fn background_color(mut self, color: Rgb<u8>) -> Self {
+        self.background_color = Some(color);
+
+        self
+    }
+
+    /// Set the action to take when overflow happens in y.
+    /// 
+    /// Default value is `Hidden`.
+    pub fn overflow_y(mut self, action: Overflow) -> Self {
+        self.overflow_y = Some(action);
+
+        self
+    }
+
+    /// Set the action to take when overflow happens in x.
+    /// 
+    /// Default value is `Hidden`.
+    pub fn overflow_x(mut self, action: Overflow) -> Self {
+        self.overflow_x = Some(action);
+
+        self
+    }
+}
+
+// This `impl` block defines methods about actual operations.
+impl ImageComposer {
+
 }
