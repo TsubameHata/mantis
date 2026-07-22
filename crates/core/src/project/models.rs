9f7b2b035704393caa7f53777ef446ffd2d05ac5
TsubameHata <2640855495@qq.com>
2026-07-23 01:30:44 +0800

    Feature: add project structure def

--- crates/core/src/project/models.rs
+++ crates/core/src/project/models.rs
@@ -1,8 +1,32 @@
-//! Defines data models in project file.
+//! Defines data models in project file, as well as project folder structure.
 //! 
-//! A Mantis project, temporarily, 
-//! consists of metadata, page images, vector masks and bitmap masks, which are packed into a folder,
-//! where individual files are arranged into subfolders and 
-//! the others (including those pointers to the individual files) are written in `json`.
+//! A Mantis project, at the current stage, 
+//! consists of metadata, page images, vector masks and bitmap masks (, as well as slices produced, for future features ), 
+//! which are packed into a folder,
+//! where individual files are arranged into subfolders 
+//! and the others (including those pointers to the individual files) are written in `json`.
 //! 
-//! The structure of the folder is defined as below:
+//! The structure of the folder, as well as data model and explanation of files, is defined as below.
+//! Folders and files need not always exist except `project.json`, and `pages.json` can exist with different name, which is referenced by `project.json`.
+//! 
+//! ```plain
+//! project.mantis/
+//!     project.json    (ProjectFile,   overall metadata, link to pages.json)
+//!     pages.json      (PagesFile,      page metadata, links to page files, slice definition, vector masks, links to bitmap masks and output slices)
+//!     pages/
+//!         page-0001.png
+//!         page-0002.png
+//!         ...
+//!         (naming need not strictly follow the convention above, since the pages are not referenced by naming convention, 
+//!             but an increasing index is advised for readability)
+//!     bitmap_masks/
+//!         a39f62.png
+//!         p28u7q.png
+//!         ...
+//!         (hash or uuid is advised for naming convention under this folder)
+//!     outputs/
+//!         page-0001-1.png
+//!         page-0001-2.png
+//!         ...
+//!         (naming convention is not strict, and not advised at the current stage of development)
+//! ``` 
